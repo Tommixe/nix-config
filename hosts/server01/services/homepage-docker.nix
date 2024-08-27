ccc9dff25753555ff20714adf39142182207743e
tommaso <tommaso@tzero.it>
2024-08-27 19:31:07 +0200

    Test docker secrets

---
+++ hosts/server01/services/homepage-docker.nix
@@ -0,0 +1,27 @@
+{ config, ... }:
+
+{
+  virtualisation.oci-containers.containers = {
+    name = "homepage";
+    image = "ghcr.io/gethomepage/homepage:latest";
+    ports = [ "3000:3000" ];
+    environment = {
+      "PUID" = "1000";
+      "PGID" = "1000";
+    };
+    volumes = [
+      "/srv/docker-apps/homepage:/app/config"
+      "/var/run/docker.sock:/var/run/docker.sock"
+    ];
+  };
+
+  sops.secrets."homepage/service.yaml" = {
+    owner = "tommaso";
+    path = "/srv/docker-apps/homepage/service.yaml";
+  };
+
+  environment.persistence = {
+    "/persist".directories = [ "/srv/docker-apps/homepage" ];
+  };
+
+}   
