b2ea53bbb2129438f4b204aa89e74d7f38b80877
tommaso <tommaso@tzero.it>
2024-09-28 11:48:46 +0200

    Removed not necessary persist file and updated secrets

--- hosts/common/global/optin-persistence.nix
+++ hosts/common/global/optin-persistence.nix
@@ -22,13 +22,6 @@
         "/srv"
         "/var/lib/nfs"
       ];
-      files = [
-        "/etc/machine-id"
-        "/etc/ssh/ssh_host_ed25519_key"
-        "/etc/ssh/ssh_host_ed25519_key.pub"
-        "/etc/ssh/ssh_host_rsa_key"
-        "/etc/ssh/ssh_host_rsa_key.pub"
-      ];
     };
   };
   programs.fuse.userAllowOther = true;
