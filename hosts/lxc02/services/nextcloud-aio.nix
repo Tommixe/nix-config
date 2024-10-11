4f65825f3f4fe6a71645fc92ab8880b107534d15
tommaso <tommaso@tzero.it>
2024-10-11 12:14:34 +0200

    Fixed typo in nextcloud volume

--- hosts/lxc02/services/nextcloud-aio.nix
+++ hosts/lxc02/services/nextcloud-aio.nix
@@ -14,7 +14,7 @@
         #extraOptions = ["--restart=always"];
         volumes = [
           "nextcloud_aio_mastercontainer:/mnt/docker-aio-config"
-          "/var/run/docker.sock:/var/run/docker.sock:roa"
+          "/var/run/docker.sock:/var/run/docker.sock:ro"
         ];
         environment = {
           APACHE_PORT = "11000";
