57d7188474821c85ad9efdcd13be7c6e8235c255
tommaso <tommaso@tzero.it>
2024-11-03 14:32:30 +0100

    Removed unused pkgs

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,5 +2,4 @@
   openrgb = import ./openrgb.nix;
   satisfactory = import ./satisfactory.nix;
   duplicacy = import ./duplicacy.nix;
-  #sitespeedio = import ./sitespeedio.nix;
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,22 +3,8 @@
 }:
 rec {
 
-  # Packages with an actual source
-  rgbdaemon = pkgs.callPackage ./rgbdaemon { };
-  shellcolord = pkgs.callPackage ./shellcolord { };
-  #trekscii = pkgs.callPackage ./trekscii { };
-  #speedtestpp = pkgs.callPackage ./speedtestpp { };
-  #lando = pkgs.callPackage ./lando { };
-  tidal-dl = pkgs.python3Packages.callPackage ./tidal-dl { };
-  #sitespeedio = pkgs.callPackage ./sitespeedio { };
-
   # Personal scripts
-  nix-inspect = pkgs.callPackage ./nix-inspect { };
-  minicava = pkgs.callPackage ./minicava { };
   pass-wofi = pkgs.callPackage ./pass-wofi { };
-  primary-xwayland = pkgs.callPackage ./primary-xwayland { };
-  wl-mirror-pick = pkgs.callPackage ./wl-mirror-pick { };
-  lyrics = pkgs.callPackage ./lyrics { };
   xpo = pkgs.callPackage ./xpo { };
 
   # My slightly customized plymouth theme, just makes the blue outline white
