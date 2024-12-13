51be70cc2a430fac60b800f3ff693d11405bb583
Tommixe <tommaso@tzero.it>
2024-12-13 12:34:19 +0100

    Sonarr insecure pkgs

--- home/user01/global/default.nix
+++ home/user01/global/default.nix
@@ -29,7 +29,16 @@ in
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
       # FIXME
-      permittedInsecurePackages = [ "openssl-1.1.1u" ];
+      #https://discourse.nixos.org/t/solved-sonarr-is-broken-in-24-11-unstable-aka-how-the-hell-do-i-use-nixpkgs-config-permittedinsecurepackages/56828/6
+      permittedInsecurePackages = [ 
+            "openssl-1.1.1u" 
+            "dotnet-runtime-wrapped-6.0.36"
+            "aspnetcore-runtime-6.0.36"
+            "aspnetcore-runtime-wrapped-6.0.36"
+            "dotnet-sdk-6.0.428"
+            "dotnet-sdk-wrapped-6.0.428"
+            "dotnet-runtime-6.0.36"
+      ];
     };
   };
 
