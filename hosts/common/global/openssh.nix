698bd0cf373fa53ef90b7ae08186bf513fe3e2cb
Tommixe <tommaso@tzero.it>
2024-12-13 10:41:33 +0100

    Removing knowhost for issue with tailscale

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -36,14 +36,14 @@ in
     ];
   };
 
-  programs.ssh = {
-    # Each hosts public key
-    knownHosts = builtins.mapAttrs (name: _: {
-      publicKeyFile = pubKey name;
-      extraHostNames = (lib.optional (name == hostName) "localhost"); # ++ # Alias for localhost if it's the same host
-      #       (lib.optionals (name == gitHost) [ "m7.rs" "git.m7.rs" ]); # Alias for m7.rs and git.m7.rs if it's the git host
-    }) hosts;
-  };
+  # programs.ssh = {
+  #   # Each hosts public key
+  #   knownHosts = builtins.mapAttrs (name: _: {
+  #     publicKeyFile = pubKey name;
+  #     extraHostNames = (lib.optional (name == hostName) "localhost"); # ++ # Alias for localhost if it's the same host
+  #     #       (lib.optionals (name == gitHost) [ "m7.rs" "git.m7.rs" ]); # Alias for m7.rs and git.m7.rs if it's the git host
+  #   }) hosts;
+  # };
 
   # Passwordless sudo when SSH'ing with keys
   security.pam.sshAgentAuth = {
