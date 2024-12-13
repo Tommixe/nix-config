e1cc052b530e7b24c049c4a2fc8ef0c0d617e0fb
Tommixe <tommaso@tzero.it>
2024-12-13 18:30:29 +0100

    Adding comment to remember

--- hosts/ws02/services/hydra/default.nix
+++ hosts/ws02/services/hydra/default.nix
@@ -91,6 +91,7 @@ in
     };
   };
 
+  #user hydra-www is not able to connect other machine via ssh because it cannot save know keys in the ver/empty folder 
   environment.persistence = {
     "/persist".directories = [ "/var/lib/hydra" 
         "/var/empty/"            
