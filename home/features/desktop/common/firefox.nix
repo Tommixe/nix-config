b05b8b6b41cfc83ee0067c97e62249a51902ffe3
Tommixe <tommaso@tzero.it>
2024-12-13 09:05:39 +0100

    remove firefox as default browser

--- home/features/desktop/common/firefox.nix
+++ home/features/desktop/common/firefox.nix
@@ -50,10 +50,12 @@ in
     };
   };
 
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = [ "firefox.desktop" ];
-    "text/xml" = [ "firefox.desktop" ];
-    "x-scheme-handler/http" = [ "firefox.desktop" ];
-    "x-scheme-handler/https" = [ "firefox.desktop" ];
-  };
+
+  # xdg.mimeApps.defaultApplications = {
+  #   "text/html" = [ "firefox.desktop" ];
+  #   "text/xml" = [ "firefox.desktop" ];
+  #   "x-scheme-handler/http" = [ "firefox.desktop" ];
+  #   "x-scheme-handler/https" = [ "firefox.desktop" ];
+  # };
+
 }
