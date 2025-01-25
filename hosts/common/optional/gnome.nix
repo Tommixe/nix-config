3976d4d01c8cb02c1ad30b4634e6ddad85a5e6f2
Tommixe <tommaso@tzero.it>
2025-01-25 15:55:57 +0100

    Gnome rdp enable issue 361163

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -16,9 +16,20 @@
     printing.enable = true;
 
   };
+
   # Fix broken stuff
   #services.avahi.enable = false;
+  # but to rally activate it 
+  # systemctl --user restart gnome-remote-desktop.service
   networking.networkmanager.enable = true;
 
-  
+  systemd.services.gnome-remote-desktop = {
+      wantedBy = [ "graphical.target" ];
+    };
+
+   #allow rdp
+  networking.firewall.allowedTCPPorts = [ 3389 ];
+  networking.firewall.allowedUDPPorts = [ 3389 ];
+
+
 }
