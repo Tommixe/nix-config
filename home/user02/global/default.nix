ed491db234bb8e48473e80cc76c717228150d107
Tommixe <tommaso@tzero.it>
2025-03-13 08:26:26 +0100

    allow unfree for user02

--- home/user02/global/default.nix
+++ home/user02/global/default.nix
@@ -15,6 +15,25 @@ in
   ] ++ (builtins.attrValues outputs.homeManagerModules);
 
 
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+      };
+  };
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = false;
+    };
+  };
+
+
   programs = {
     home-manager.enable = true;
   };
