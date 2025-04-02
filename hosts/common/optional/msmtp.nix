67be2e91384e5a0fbeb41fa649ac7b7f22602a2b
Tommixe <tommaso@tzero.it>
2025-04-02 17:07:53 +0200

    Adding global var at config level

--- hosts/common/optional/msmtp.nix
+++ hosts/common/optional/msmtp.nix
@@ -1,9 +1,6 @@
 { config, ... }:
-let
-  global-var = builtins.extraBuiltins.sopsImportEncrypted ../global-var.nix.sops;
-in
 {
-
+  
   programs.msmtp = {
     enable = true;
     accounts = {
@@ -12,11 +9,11 @@ in
         tls = true;
         tls_starttls = false;
         tls_trust_file = "/etc/ssl/certs/ca-certificates.crt";
-        host = global-var.msmtp-host;
+        host = config.global-var.msmtp-host;
         port = 465; #"cat ${config.sops.secrets.msmtp-port.path}";
-        user = global-var.msmtp-user;
+        user = config.global-var.msmtp-user;
         passwordeval = "cat ${config.sops.secrets.msmtp-passwordeval.path}";
-        from = global-var.msmtp-from;
+        from = config.global-var.msmtp-from ;
       };
     };
   };
@@ -26,4 +23,5 @@ in
       sopsFile = ../secrets.yaml;
     };
   };
+
 }
