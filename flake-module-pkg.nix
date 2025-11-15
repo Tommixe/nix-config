b289da1539e1664e412dd144d13f3b0ef9b44141
Tommixe <tommaso@tzero.it>
2025-11-15 14:16:35 +0100

    delete uncessary files

--- flake-module-pkg.nix
+++
@@ -1,7 +0,0 @@
-#Then create a module for your packages:
-{ self, inputs, ... }: {
-  perSystem = { pkgs, ... }: {
-    packages = import ../pkgs { inherit pkgs; };
-    devShells = import ../shell.nix { inherit pkgs; };
-  };
-}
