90ddd7ec25718125d1ad30aef5dff3eb3e8a56d7
Tommixe <tommaso@tzero.it>
2025-11-20 22:54:05 +0100

    remove pipe operator

--- modules/nixos/flake-parts/hydra-jobs.nix
+++ modules/nixos/flake-parts/hydra-jobs.nix
@@ -14,15 +14,14 @@ in
     in
     {
       hydraJobs = {
-        hosts =
-          outputs.nixosConfigurations
-          # remove iso, I don't need it every build
-          |> lib.filterAttrs (n: _v: !builtins.elem n [ "iso" ])
-          # add to my jobs dashboard and set up evaluation error notifications
-          |> lib.mapAttrs (
-            _: cfg:
-            lib.addMetaAttrs { maintainers = [ "email@nt.it" ]; } cfg.config.system.build.toplevel
-          );
+        hosts = outputs.nixosConfigurations;
+        # remove iso, I don't need it every build
+        #|> lib.filterAttrs (n: _v: !builtins.elem n [ "iso" ])
+        # add to my jobs dashboard and set up evaluation error notifications
+        #|> lib.mapAttrs (
+        #  _: cfg:
+        #  lib.addMetaAttrs { maintainers = [ "email@nt.it" ]; } cfg.config.system.build.toplevel
+        # );
       };
     };
 }
