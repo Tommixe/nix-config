cc0a4df08582179d9c862c30067e84d55327798e
Tommixe <tommaso@tzero.it>
2025-11-21 12:09:38 +0100

    hydraJobs fix

--- modules/nixos/flake-parts/hydra-jobs.nix
+++ modules/nixos/flake-parts/hydra-jobs.nix
@@ -4,24 +4,22 @@
   inputs,
   ...
 }:
-let
-  inherit (inputs.nixpkgs) lib;
-in
 {
   flake =
     let
       inherit (self) outputs;
+      inherit (inputs.nixpkgs.lib) filterAttrs mapAttrs elem;
+
+      notBroken = pkg: !(pkg.meta.broken or false);
+      hasPlatform = sys: pkg: elem sys (pkg.meta.platforms or [ ]);
+      filterValidPkgs = sys: pkgs: filterAttrs (_: pkg: hasPlatform sys pkg && notBroken pkg) pkgs;
+      getCfg = _: cfg: cfg.config.system.build.toplevel;
     in
     {
+
       hydraJobs = {
-        hosts = outputs.nixosConfigurations;
-        # remove iso, I don't need it every build
-        #|> lib.filterAttrs (n: _v: !builtins.elem n [ "iso" ])
-        # add to my jobs dashboard and set up evaluation error notifications
-        #|> lib.mapAttrs (
-        #  _: cfg:
-        #  lib.addMetaAttrs { maintainers = [ "email@nt.it" ]; } cfg.config.system.build.toplevel
-        # );
+        pkgs = mapAttrs filterValidPkgs outputs.packages;
+        hosts = mapAttrs getCfg outputs.nixosConfigurations;
       };
     };
 }
