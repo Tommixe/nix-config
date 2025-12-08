b7cd7b6401169cb68c8d0488c71d8558495a2a84
Tommixe <tommaso@tzero.it>
2025-12-08 09:46:02 +0100

    Added pfatch and ranger modules

---
+++ modules/home-manager/pfetch.nix
@@ -0,0 +1,10 @@
+{
+  flake.modules.homeManager.pfetch =
+    { pkgs, ... }:
+    {
+      home = {
+        packages = with pkgs; [ pfetch ];
+        sessionVariables.PF_INFO = "ascii title os kernel uptime shell term desktop scheme palette";
+      };
+    };
+}
