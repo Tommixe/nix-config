db751972a30f32ab6cbfbbe3a3b82905e5be8991
Tommixe <tommaso@tzero.it>
2026-01-04 12:01:21 +0100

    Moved home manager common options in a single module

--- modules/users/user01/home-manager-nixoshost-user01.nix
+++ modules/users/user01/home-manager-nixoshost-user01.nix
@@ -6,37 +6,24 @@
 }:
 {
 
-  
   flake.modules.nixos.home-manager-nixoshost-user01 =
-    { config, ... }:
-    let
+   { config, ... }:
+   let
       inherit (config.networking) hostName;
       userName = "user01";
     in
     {
-      
+      imports = [ inputs.self.modules.nixos.home-manager ];
+
       home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        backupFileExtension = "hm-backup";
 
         users.${userName}.imports = [
-          #topLevel.config.flake.modules.homeManager.base
+
           inputs.self.modules.homeManager."home-manager-${userName}" # User specific home manager module
           (inputs.self.modules.homeManager."host_${hostName}_${userName}" or { }) # Host specific home manager modules for the user
-          # {
-          #   age = {
-          #     identityPaths = [ config.age.secrets.agenix-home-secret-key.path ];
-          #     rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBE+2at1NN5ahYloIOYXyEhGi6lRN4PoapQz6CNoTo0r";
-          #   };
-          # }
+
         ];
 
-        extraSpecialArgs = {
-          inputs = inputs;
-          configName = "${hostName}";
-          nhSwitchCommand = "nh os switch";
-        };
       };
 
       #custom.imp.homeManager.directories = [ "/home/${config.home.username}" ];
--- modules/users/user02/home-manager-nixoshost-user02.nix
+++ modules/users/user02/home-manager-nixoshost-user02.nix
@@ -8,28 +8,25 @@
 
   
   flake.modules.nixos.home-manager-nixoshost-user02 =
-    { config, ... }:
+    { config,  ... }:
     let
       inherit (config.networking) hostName;
       userName = "user02";
     in
     {
       
+            imports = [ inputs.self.modules.nixos.home-manager ];
+
+
       home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        backupFileExtension = "hm-backup";
+        
 
         users.${userName}.imports = [
            inputs.self.modules.homeManager."home-manager-${userName}" # User specific home manager module
           (inputs.self.modules.homeManager."host_${hostName}_${userName}" or { }) # Host specific home manager modules for the user
         ];
 
-        extraSpecialArgs = {
-          inputs = inputs;
-          configName = "${hostName}";
-          nhSwitchCommand = "nh os switch";
-        };
+        
       };
 
       #custom.imp.homeManager.directories = [ "/home/${config.home.username}" ];
