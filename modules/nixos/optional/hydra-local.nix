95e498f315ebb19c7a92c867c0a40b102015cbfd
Tommixe <tommaso@tzero.it>
2026-04-18 11:33:55 +0200

    Hydra local force listenhost

--- modules/nixos/optional/hydra-local.nix
+++ modules/nixos/optional/hydra-local.nix
@@ -4,7 +4,7 @@
 
     {
 
-      config,
+      lib,
       inputs,
       ...
     }:
@@ -17,7 +17,7 @@
       services = {
         hydra = {
           hydraURL = "https://ws02hydra.tzero.it";
-          listenHost = "0.0.0.0";        
+          listenHost = lib.mkForce "0.0.0.0";        
         };
         
       };
