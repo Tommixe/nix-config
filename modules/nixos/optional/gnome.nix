9ce46d861f658d6ab903e42b3cf3e8f2694f0351
Tommixe <tommaso@tzero.it>
2026-08-19 09:51:09 +0200

    qt gnome

--- modules/nixos/optional/gnome.nix
+++ modules/nixos/optional/gnome.nix
@@ -1,32 +1,46 @@
 {
 
-  flake.modules.nixos.gnome = {
-    services = {
-      desktopManager.gnome = {
+  flake.modules.nixos.gnome =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      services = {
+        desktopManager.gnome = {
           enable = true;
-        };      
-      displayManager.gdm = {
+        };
+        displayManager.gdm = {
           enable = true;
           autoSuspend = false;
         };
-      geoclue2.enable = true;
-      gnome.gnome-remote-desktop.enable = true;
-      printing.enable = true;
+        geoclue2.enable = true;
+        gnome.gnome-remote-desktop.enable = true;
+        printing.enable = true;
+      };
+
+      environment.systemPackages = [
+        # Themes the app titlebars
+        pkgs.qadwaitadecorations
+        pkgs.qadwaitadecorations-qt6
+        # Themes the apps
+        pkgs.qgnomeplatform
+        pkgs.qgnomeplatform-qt6
+      ];
+
+      # Fix broken stuff
+      #services.avahi.enable = false;
+      # but to rally activate it
+      # systemctl --user restart gnome-remote-desktop.service
+      networking.networkmanager.enable = true;
+
+      systemd.services.gnome-remote-desktop = {
+        wantedBy = [ "graphical.target" ];
+      };
+
+      #allow rdp
+      networking.firewall.allowedTCPPorts = [ 3389 ];
+      networking.firewall.allowedUDPPorts = [ 3389 ];
     };
-
-    # Fix broken stuff
-    #services.avahi.enable = false;
-    # but to rally activate it
-    # systemctl --user restart gnome-remote-desktop.service
-    networking.networkmanager.enable = true;
-
-    systemd.services.gnome-remote-desktop = {
-      wantedBy = [ "graphical.target" ];
-    };
-
-    #allow rdp
-    networking.firewall.allowedTCPPorts = [ 3389 ];
-    networking.firewall.allowedUDPPorts = [ 3389 ];
-  };
 
 }
