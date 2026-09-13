d2098d568d167e2e2b1bfcf7b15e5ad218cdcf4c
Tommixe <tommaso@tzero.it>
2026-09-13 15:59:17 +0200

    Restore auto-upgrade to main branch

--- modules/nixos/base/auto-upgrade.nix
+++ modules/nixos/base/auto-upgrade.nix
@@ -1,18 +1,18 @@
 {
-  flake.modules.nixos.auto-upgrade = 
-    {inputs, config, ...}: 
+  flake.modules.nixos.auto-upgrade =
+    { inputs, config, ... }:
     {
-    imports = [ (inputs.self.modules.nixos.hydraAutoUpgrade) ];
+      imports = [ (inputs.self.modules.nixos.hydraAutoUpgrade) ];
 
-    system.hydraAutoUpgrade = {
-      # Only enable if not dirty
-      enable = inputs.self ? rev;
-      dates = "hourly";
-      instance = "https://hydracloud.tzero.it";
-      project = "flakeparts";
-      jobset = "flakeparts";
-      job = "hosts.${config.networking.hostName}";
-      oldFlakeRef = "self";
+      system.hydraAutoUpgrade = {
+        # Only enable if not dirty
+        enable = inputs.self ? rev;
+        dates = "hourly";
+        instance = "https://hydracloud.tzero.it";
+        project = "nix-config";
+        jobset = "main";
+        job = "hosts.${config.networking.hostName}";
+        oldFlakeRef = "self";
+      };
     };
-  };
 }
